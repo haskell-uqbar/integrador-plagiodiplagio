cf968fffb1084d020ff185d5e4412a2b73f5c77e
asanzo <alfredo.sanzo@gmail.com>
2025-05-02 12:42:08 -0300

    cambio un toque el orden y los nombres ppales

--- plagioHaskell.hs
+++ plagioHaskell.hs
@@ -64,6 +64,9 @@ todasLasLetrasYNumeros = ['a'..'z']++['A'..'Z'] ++ "0123456789 "
 
 type FormaDeteccion = String ->  String ->  Bool
 
+esPlagio :: Obra -> Obra -> FormaDeteccion -> Bool
+esPlagio plagio obraOriginal deteccion = fecha plagio > fecha obraOriginal && deteccion (contenido plagio) (contenido obraOriginal)  
+
 --a
 copiaLiteral :: FormaDeteccion
 copiaLiteral texto textoOriginal = versionCruda texto == versionCruda  textoOriginal
@@ -94,11 +97,9 @@ botB = UnBot [empiezaIgual 10, leAgregaronIntro] "botter"
 
 --5. Un bot detecta si una obra es plagio de otra si verifica alguna de las formas de detección que maneja.
 
-deteccion :: Obra -> Obra -> FormaDeteccion -> Bool
-deteccion obra obraOriginal forma = fecha obra > fecha obraOriginal && forma (contenido obra) (contenido obraOriginal)  
 
 esPlagioDeEstaObra :: Bot -> Obra -> Obra -> Bool
-esPlagioDeEstaObra bot obra obraOriginal = any (deteccion obra obraOriginal)  (formas bot)
+esPlagioDeEstaObra bot obra obraOriginal = any (esPlagio obra obraOriginal)  (formas bot)
 
 --6. Dado un conjunto de autores y un bot, detectar si es una cadena de plagiadores. Es decir, el segundo plagió al primero, el tercero al segundo, y así. Se considera que un autor plagió a otro cuando alguna de sus obras es plagio de alguna de las del otro según el bot.
 
@@ -140,25 +141,25 @@ obraInfinita2 = UnaObra (repeat 'a') 2025
 
 {-
 
-Suponiendo una consulta como: deteccion obraA obraInfinita copiaLiteral
+Suponiendo una consulta como: esPlagio obraA obraInfinita copiaLiteral
 No importa cuál sea la forma de detección, como la fecha de la obra que se pregunta si es plagio es anterior, no se cumple y corta diciendo False, por Lazy evaluation no es necesario seguir evaluando el otro lado del &&.
 
 Ahora veamos los casos donde se cumple que la fecha es posterior:
 
 copiaLiteral:
-- Suponiendo la consulta: deteccion obraInfinita obraA copiaLiteral
+- Suponiendo la consulta: esPlagio obraInfinita obraA copiaLiteral
 da False, por Lazy Evaluation. Al evaluar la igualdad de contenidos no necesita evaluar toda la lista infinita para saber que los strings son distintos, con los primeros caracteres alcanza.
-- Pero si consulto deteccion obraInfinita2 obraInfinita copiaLiteral, se cuelga, porque para saber si dos strings iguales son iguales necesita recorrerlos todos, aún con lazy evaluation.
+- Pero si consulto esPlagio obraInfinita2 obraInfinita copiaLiteral, se cuelga, porque para saber si dos strings iguales son iguales necesita recorrerlos todos, aún con lazy evaluation.
 
 empiezaIgual:
-- Suponiendo la consulta: deteccion obraInfinita obraA empiezaIgual
+- Suponiendo la consulta: esPlagio obraInfinita obraA empiezaIgual
 Entonces da False, pues verifica con take que sean iguales los contenidos y eso da false. Por Lazy evaluation no es necesario evaluar la lista infinita para el take.
-- Suponiendo la consulta: deteccion obraInfinita2 obraInfinita empiezaIgual
+- Suponiendo la consulta: esPlagio obraInfinita2 obraInfinita empiezaIgual
 Ahí se cuelga, porque nunca llega a comparar las longitudes de los contenidos, aún con lazy evaluation. Es decir, aún si una es infinita y la otra empieza igual, jamás cortará.
 
 leAgregaronIntro:
-- Suponiendo la consulta: deteccion obraInfinita obraA leAgregaronIntro
+- Suponiendo la consulta: esPlagio obraInfinita obraA leAgregaronIntro
 Aún teniendo lazy evaluation, para el calcular el length del contenido de la obra infinita se cuelga, antes de poder hacer el drop.
-- Ahora, si hacemos al revés: deteccion obraA obraInfinita leAgregaronIntro
+- Ahora, si hacemos al revés: esPlagio obraA obraInfinita leAgregaronIntro
 Se colgaría, pues se pide hacer un ultimosElementos, que a su vez necesita el length de la lista infinita, no hay Lazy evaluation que lo salve.
 -}
